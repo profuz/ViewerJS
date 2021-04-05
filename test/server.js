8ddb595ae05bc748d1c1ceaa2f254b1bf8fc0e4f
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-04-05 00:00:00 +0000

    Added OpenDocument test files.

--- test/server.js
+++ test/server.js
@@ -11,6 +11,9 @@ console.log(`Listening on port 5581.
  Try one of the following tests:
   - http://localhost:5581/index.html#/files/image.png
   - http://localhost:5581/index.html#/files/pdf.pdf
+  - http://localhost:5581/index.html#/files/presentation.odp
+  - http://localhost:5581/index.html#/files/spreadsheet.ods
+  - http://localhost:5581/index.html#/files/text.odt
   - http://localhost:5581/index.html#/files/text.txt
   - http://localhost:5581/index.html#/files/video.mp4
   - http://localhost:5581/index.html#/files/video.webm
