3dd27490f245f8bfb16ccfb847d3aa8b13b54bc7
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-04-05 00:00:00 +0000

    Fixed server message.

--- test/server.js
+++ test/server.js
@@ -7,7 +7,7 @@ app.use('/', express.static('./release'));
 app.use('/files', express.static('./test/files'));
 app.listen(5581);
 
-console.log(`Listening on port 81.
+console.log(`Listening on port 5581.
  Try one of the following tests:
   - http://localhost:5581/index.html#/files/pdf-test.pdf
   - http://localhost:5581/index.html#/files/small.mp4
@@ -15,5 +15,5 @@ console.log(`Listening on port 81.
   - http://localhost:5581/index.html#/files/test.png
   - http://localhost:5581/index.html#/files/text.txt
   - http://localhost:5581/index.html#/files/unknown.zfx
-  
-  `);
+
+`);
